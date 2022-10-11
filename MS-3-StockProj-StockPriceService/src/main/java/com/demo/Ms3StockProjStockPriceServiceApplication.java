f34acd2c9e64e44279d20cb229adbf404ae21728
tl <tl@gmail.com>
2022-10-11 20:46:46 +0530

    eureka client added

--- MS-3-StockProj-StockPriceService/src/main/java/com/demo/Ms3StockProjStockPriceServiceApplication.java
+++ MS-3-StockProj-StockPriceService/src/main/java/com/demo/Ms3StockProjStockPriceServiceApplication.java
@@ -2,12 +2,14 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Ms3StockProjStockPriceServiceApplication {
 
 	public static void main(String[] args) {
